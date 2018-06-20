4eb7bd70a80e59450e8507918f8db871da82bba7
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-20 20:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
  
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const currentUser = 'Edsger Dijkstra';
- const shortGreeting = 'ED';
