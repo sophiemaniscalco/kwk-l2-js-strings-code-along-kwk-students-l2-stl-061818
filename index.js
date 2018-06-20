511a5830d0b842df393c325ef4263773d71f1151
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-20 20:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
  
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
- 
-const shortGreeting = 'Welcome, ' ;
- 
- 
+
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
